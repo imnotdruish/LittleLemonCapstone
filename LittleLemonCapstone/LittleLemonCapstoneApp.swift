a594953108ce597602f4196e6a48944f1115895a
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-03-02 15:50:52 -0500

    Initial Commit

---
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by Dan June on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
