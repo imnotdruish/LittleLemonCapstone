12ad1f1e6ccbf66e34011b9684c9ad51df03f45f
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-03-03 16:02:19 -0500

    Touch ups for TextFields and Icons

---
+++ LittleLemonCapstone/Views/AvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarView.swift
+//  LittleLemonCapstone
+//
+//  Created by Dan June on 3/3/25.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvatarView()
+}
